3fbfcb28978486a59638ffc872edf5e496efb3b0
Billy Lin <billylin@global.cadence.com>
2024-02-01 22:07:09 +0800

    lab03

--- lab03_counter/src/top.sv
+++ lab03_counter/src/top.sv
@@ -3,13 +3,19 @@
 */
 module top;
 	bit clk;    //clock
-	logic [1:0] cnt_2bits = 2'd0; 
-	logic [2:0] cnt_3bits = 3'd0; 
-	int cnt_int = 0;
+	logic [1:0] cnt_2bits = 2'd0;  //counter packed
+	logic [2:0] cnt_3bits  = 3'd0; //packed. cannot be unpacked
+	int cnt_int = 0; //32-bits
+	byte my_byte; //8-bits, concat by sig1 and sig2
+	
+	bit [1:0] sig1 = 2'b10;
+	bit [5:0] sig2 = 6'b111111;
+	
+	//assign my_byte = {sig1, sig2};  conti-assignment
 	
 	always #5 clk = ~clk; //clock with period 10sec
 
-	always @(posedge clk) begin
+	always @(edge clk) begin
 		cnt_2bits <= cnt_2bits + 1;
     end
 	
@@ -24,9 +30,12 @@ module top;
     initial begin
         $dumpfile("top.vcd");
 		$dumpvars;
-
-		#1000;
+		my_byte = {sig1, sig2}; //MSB LSB
+		#10;
+		my_byte = {sig2, sig1}; //MSB LSB
+		#1000; 
 		$display("at time = ", $time, " end of the sim");
 		$finish;    
     end
 endmodule
+
